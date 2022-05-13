a3c2ae945b596df2cade83140e9766650d4f9ef6
Raphael O. Melo <raphael.o.melo@outlook.com>
2022-05-12 21:02:07 -0300

    First tests with states
    When user click on h2, two new tasks appears on list

--- alura-studies/src/components/List/index.tsx
+++ alura-studies/src/components/List/index.tsx
@@ -1,25 +1,34 @@
+import { log } from "console";
+import { useState } from "react";
 import Item from "./Item";
 import list from './list.module.scss';
 
 export default function List() {
-    const tasks = [
-        {
-            task: "React",
-            time: "02:00:00" 
-        },
-        {
-            task: "Javascript",
-            time: "01:00:00" 
-        },
-        {
-            task: "Typescript",
-            time: "03:00:00" 
-        },
-    ];
+
+    const [tasks, setTasks] = useState(
+        [
+            {
+                task: "React",
+                time: "02:00:00" 
+            },
+            {
+                task: "Javascript",
+                time: "01:00:00" 
+            },
+            {
+                task: "Typescript",
+                time: "03:00:00" 
+            },
+        ]
+    );
+
+
     
     return(
         <aside className={list.listaTarefas}>
-            <h2>Estudos do dia</h2>
+            <h2 onClick={() => {
+                setTasks([...tasks, {task: "Nova tarefa 1", time: "01:02:03"}, {task: "Nova tarefa 2", time: "04:05:06"}])
+            }}>Estudos do dia</h2>
             <ul>
                 {tasks.map((item, key) => (
                     <Item key={key} task={item.task} time={item.time} />
