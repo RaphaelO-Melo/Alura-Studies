ae04e0013e1278d7733b5a19e0703ede0412ddc6
Raphael O. Melo <raphael.o.melo@outlook.com>
2022-05-12 21:42:57 -0300

    Using states to get input values
    Getting values and printing in console

--- alura-studies/src/components/Form/index.tsx
+++ alura-studies/src/components/Form/index.tsx
@@ -3,20 +3,47 @@ import Button from "../Button";
 import form from './form.module.scss';
 
 class Form extends React.Component {
+
+    state = {
+        'name': '',
+        'time': '00:00:00'
+    }
+
+    addTask(event: React.FormEvent){
+        event.preventDefault();
+        console.log(this.state);
+    }
+
     render(){
         return(
-            <form className={form.novaTarefa}>
+            <form className={form.novaTarefa} onSubmit={this.addTask.bind(this)}>
                 <div className={form.inputContainer}>
                     <label htmlFor="tarefa">
                         Adicione um novo estudo
                     </label>
-                    <input type="text" name="tarefa" id="tarefa" placeholder="O que você quer estudar" required/>
+                    <input
+                    type="text" 
+                    name="tarefa" 
+                    id="tarefa" 
+                    placeholder="O que você quer estudar"
+                    value={this.state.name}
+                    onChange={event => this.setState({...this.state, name: event.target.value})}
+                    required/>
                 </div>
                 <div className={form.inputContainer}>
                     <label htmlFor="tempo">
                         Tempo
                     </label>
-                    <input type="time" step="1" name="tempo" id="tempo" min="00:00:00" max="01:30:00" required/>
+                    <input 
+                    type="time" 
+                    step="1"
+                    name="tempo" 
+                    id="tempo" 
+                    min="00:00:00" 
+                    max="01:30:00" 
+                    value={this.state.time} 
+                    onChange={event => this.setState({...this.state, time: event.target.value})} 
+                    required/>
                 </div>
                 <Button>Adicionar</Button>
             </form>
