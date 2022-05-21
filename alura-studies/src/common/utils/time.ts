37609255591ee4889ef3de75d6beb669284e7289
Raphael O. Melo <raphael.o.melo@outlook.com>
2022-05-20 21:55:01 -0300

    NaN in chronometer solved

--- alura-studies/src/common/utils/time.ts
+++ alura-studies/src/common/utils/time.ts
@@ -1,10 +1,14 @@
 export function timeToSeconds(time: string){
     
-    const[hour = '0', minutes = '0', seconds = '0'] = time.split(':');
+    var[hours = '0', minutes = '0', seconds = '0'] = time.split(':');
     
-    const hourInSeconds = Number(hour) * 3600;
+    if(hours == 'undefined'){
+        hours = '0';
+    }
+
+    const hourInSeconds = Number(hours) * 3600;
     const minutesInSeconds = Number(minutes) * 60;
-    
+
     return hourInSeconds + minutesInSeconds + Number(seconds);
 
 }
